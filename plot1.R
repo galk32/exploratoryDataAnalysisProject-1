b319e749c81ff28b09a8559eb966d4872e280835
galk32 <49902643+galk32@users.noreply.github.com>
2019-04-23 21:24:09 +0300

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,13 @@
+hpc <- read.csv("~/Coursera/Coursera - Exploratory Data Analysis - R/week1/project 1/Coursera_project1/household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
+head(hpc)
+str(hpc)
+hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
+hpc$Time <- strptime(hpc$Time, format = "%H:%M:%S")
+hpc$Global_active_power <- as.numeric(hpc$Global_active_power)
+
+#making the plot
+png(filename = "plot.1.png", width = 480, height =480, units = "px")
+plotSample <- subset(hpc, Date == "2007-02-01" | Date == "2007-02-02")
+str(plotSample)
+hist(plotSample$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()

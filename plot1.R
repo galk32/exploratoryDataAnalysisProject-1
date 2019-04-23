9af984041bd9687d4eadec2fa15f3f884f6c1977
galk32 <49902643+galk32@users.noreply.github.com>
2019-04-23 21:27:57 +0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,4 @@
+#importing the txt file
 hpc <- read.csv("~/Coursera/Coursera - Exploratory Data Analysis - R/week1/project 1/Coursera_project1/household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
 head(hpc)
 str(hpc)
@@ -6,8 +7,7 @@ hpc$Time <- strptime(hpc$Time, format = "%H:%M:%S")
 hpc$Global_active_power <- as.numeric(hpc$Global_active_power)
 
 #making the plot
-png(filename = "plot.1.png", width = 480, height =480, units = "px")
 plotSample <- subset(hpc, Date == "2007-02-01" | Date == "2007-02-02")
-str(plotSample)
+png(filename = "plot.1.png", width = 480, height =480, units = "px")
 hist(plotSample$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
 dev.off()

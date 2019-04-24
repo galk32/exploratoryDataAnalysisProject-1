3edacfd1238d21cc29f2f6e8a89c29b333724413
galk32 <49902643+galk32@users.noreply.github.com>
2019-04-24 13:31:14 +0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,14 +1,12 @@
 hpc <- read.csv("~/Coursera/Coursera - Exploratory Data Analysis - R/week1/project 1/Coursera_project1/household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
-
 hpc$dateTime <- paste(hpc$Date, hpc$Time, sep= " ")
 hpc$dateTime <-strptime(hpc$dateTime, format = "%d/%m/%Y %H:%M:%S") 
 hpc$Global_active_power <- as.numeric(hpc$Global_active_power)
-
-#making the plot
+#open the device
 png(filename = "plot.2.png", width = 480, height =480, units = "px")
 #making the sample
 plotSample <- subset(hpc, dateTime >= "2007-02-01 00:00:00" & dateTime <= "2007-02-02 23:59:59")
-str(plotSample)
+#making the plot
 with(plotSample, plot(dateTime, Global_active_power, type = "l",xlab = "", ylab = "Global Active Power (kilowatts)"))
 dev.off()
 

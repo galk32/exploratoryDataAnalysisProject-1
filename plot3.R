d79c93a1babd32cbc105fdfc51f46ce06324ff91
galk32 <49902643+galk32@users.noreply.github.com>
2019-04-24 18:31:39 +0300

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,19 @@
+hpc <- read.csv("~/Coursera/Coursera - Exploratory Data Analysis - R/week1/project 1/Coursera_project1/household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
+
+hpc$dateTime <- paste(hpc$Date, hpc$Time, sep= " ")
+hpc$dateTime <-strptime(hpc$dateTime, format = "%d/%m/%Y %H:%M:%S") 
+str(hpc)
+hpc$Sub_metering_1 <- as.numeric(hpc$Sub_metering_1)
+hpc$Sub_metering_2 <- as.numeric(hpc$Sub_metering_2)
+#making the plot
+png(filename = "plot.3.png", width = 480, height =480, units = "px")
+#making the sample
+plotSample <- subset(hpc, dateTime >= "2007-02-01 00:00:00" & dateTime <= "2007-02-02 23:59:59")
+str(plotSample)
+plot(plotSample$dateTime,plotSample$Sub_metering_1, col ="black", type = "l", xlab = " ", ylab = "Energy sub metering")
+lines(plotSample$dateTime, plotSample$Sub_metering_2,col="red", type = "l")
+lines(plotSample$dateTime, plotSample$Sub_metering_3,col="blue", type = "l")      
+legend("topright",lty = 1,col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3") )
+
+dev.off()
+
